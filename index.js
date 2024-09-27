1811b0c3ad0fee93ee882dde03a3ebdd53a965a2
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2024-09-27 11:44:59 -0400

    i did it wrong so i fixed it

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const SERVER_PORT = process.env.port || 3000;
+var url = require('url');
 
 // http://localhost:3000/
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+// http://localhost:3000/hello
+app.get('/hello', (req, res) => {
+    res.send('Hello Express JS');
+})
+
+// http://localhost:3000/user
+app.get('/user', (req, res) => {
+    var firstName = req.query.firstname || "Pritesh";
+    var lastName = req.query.lastname || "Patel";
+
+
+    res.json({firstName, lastName});
+
+});
+
+// http://localhost:3000/user/firstname/lastname
+app.post('/user/:firstname/:lastname', (req, res) => {
+    const {firstname, lastname} = req.params;
+    res.json({firstname, lastname});
 })
 
 // http://localhost:3000/about
